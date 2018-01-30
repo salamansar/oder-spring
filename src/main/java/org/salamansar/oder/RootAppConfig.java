e4e21d363ae14f40fd5a464b6a505e8ac0f8f787
salamansar <salamansar@gmail.com>
2018-01-30 22:05:56 +0300

    configure of HSQL db connection

--- src/main/java/org/salamansar/oder/RootAppConfig.java
+++ src/main/java/org/salamansar/oder/RootAppConfig.java
@@ -1,12 +1,50 @@
 package org.salamansar.oder;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
  * @author Salamansar
  */
 @Configuration
+@EnableJpaRepositories("org.salamansar.oder.core.dao")
+@EnableTransactionManagement
 public class RootAppConfig {
-
+    
+    @Bean
+    public DataSource embeddedDB() {
+        return new EmbeddedDatabaseBuilder()
+                .build();
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean emFactory() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setGenerateDdl(true);
+        
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(embeddedDB());
+        factory.setJpaVendorAdapter(adapter);
+        factory.setPackagesToScan("org.salamansar.oder.domain");
+        return factory;
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(emf);
+        return manager;
+    }
+    
+    
 }
---
+++ src/main/java/org/salamansar/oder/core/dao/IncomeDao.java
@@ -0,0 +1,12 @@
+package org.salamansar.oder.core.dao;
+
+import org.salamansar.oder.core.domain.Income;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Salamansar
+ */
+public interface IncomeDao extends CrudRepository<Income, Long> {
+
+}
